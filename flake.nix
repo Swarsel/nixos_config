d91969f63a4a0f7432472242f029bf5e12b56b04
Swarsel <leon.schwarzaeugl@gmail.com>
2023-08-25 18:03:19 +0200

    update with more config

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   		{
     		home-manager.useGlobalPkgs = true;
     		home-manager.useUserPackages = true;
-    		home-manager.users.swarsel = import /home/swarsel/.config/home-manager/home.nix;
+    		home-manager.users.swarsel = import ./home.nix;
   		}
         ];
       };
