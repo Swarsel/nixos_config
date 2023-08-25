3696c18cc030981a87da5de339a94fe2de7a696a
Swarsel <leon.schwarzaeugl@gmail.com>
2023-08-25 19:32:00 +0200

    maybe

--- flake.nix
+++ flake.nix
@@ -3,38 +3,34 @@
   description = "Config flake";
 
   inputs = {
-  	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  	nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    	# You can access packages and modules from different nixpkgs revs
+    	# at the same time. Here's an working example:
+    	nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
       	home-manager = {
-		url = "github:nix-community/home-manager";
+		url = "github:nix-community/home-manager/release-23.05";
         	inputs.nixpkgs.follows = "nixpkgs";
     	};
   };
 
   outputs = { self, nixpkgs, home-manager }: {
     	nixosConfigurations = {
-    		nixosadminpc = nixpkgs.lib.nixosSystem {
+    		nixos = nixpkgs.lib.nixosSystem {
         		system = "x86_64-linux";
         		modules = [
           			./lenovo/configuration.nix
 				./lenovo/greetd.nix
-	  			home-manager.nixosModules.home-manager {
-    					home-manager.useGlobalPkgs = true;
-    					home-manager.useUserPackages = true;
-    					home-manager.users.swarsel = import lenovo/home.nix;
-  				}
         		];
 		};
-    
+	};    
     	# some future setup that only uses home manager on a non NixOS machine
     	
-		surface = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
+	homeConfigurations = {
+		"swarsel@nixos" = home-manager.lib.homeManagerConfiguration {
+			pkgs = nixpkgs.legacyPackages.x86_64-linux;
+			#extraSpecialArgs = { inherit inputs outputs; };
 			modules = [ 
-				home-manager.nixosModules.home-manager {
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.swarsel = import surface/home.nix;
-				}
+				./lenovo/home.nix
 			];
 		};
 	};                	
