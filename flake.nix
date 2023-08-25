c537444b640162a96d2b12e32d7ecdf1ff5449bb
Swarsel <leon.schwarzaeugl@gmail.com>
2023-08-25 18:22:53 +0200

    finish future second setup framework

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 # /etc/nixos/flake.nix
 {
-  description = "flake for Lenovo Nixos build";
+  description = "Config flake";
 
   inputs = {
   	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -23,16 +23,19 @@
   				}
         		];
 		};
-      	};
     
     	# some future setup that only uses home manager on a non NixOS machine
-    	homeConfigurations = {
-            	homeMachine = home-manager.lib.homeManagerConfiguration {
-                	# Note: I am sure this could be done better with flake-utils or something
-                	pkgs = import nixpkgs { system = "x86_64-linux"; };
-                	modules = [ ./home.nix ]; # Defined later
-            	};
-        };
-    
+    	
+		surface = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [ 
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.swarsel = import ./home.nix;
+				}
+			];
+		};
+	};                	
   };
 }
