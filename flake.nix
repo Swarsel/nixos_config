9577d79eafb238d888ff71bf40b1c74de67938cb
Swarsel <leon.schwarzaeugl@gmail.com>
2023-08-25 17:59:50 +0200

    update with home.nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,25 @@
   description = "flake for Lenovo Nixos build";
 
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      home-manager = {
+	url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
       nixosadminpc = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+	  home-manager.nixosModules.home-manager
+  		{
+    		home-manager.useGlobalPkgs = true;
+    		home-manager.useUserPackages = true;
+    		home-manager.users.swarsel = import /home/swarsel/.config/home-manager/home.nix;
+  		}
         ];
       };
     };
