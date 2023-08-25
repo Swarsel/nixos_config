702a566f10b837c5325605790a1f4e45b2978cb2
Swarsel <leon.schwarzaeugl@gmail.com>
2023-08-25 18:16:52 +0200

    add future second config

--- flake.nix
+++ flake.nix
@@ -3,27 +3,36 @@
   description = "flake for Lenovo Nixos build";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-      home-manager = {
-	url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
+  	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      	home-manager = {
+		url = "github:nix-community/home-manager";
+        	inputs.nixpkgs.follows = "nixpkgs";
+    	};
   };
 
   outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations = {
-      nixosadminpc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-	  home-manager.nixosModules.home-manager
-  		{
-    		home-manager.useGlobalPkgs = true;
-    		home-manager.useUserPackages = true;
-    		home-manager.users.swarsel = import ./home.nix;
-  		}
-        ];
-      };
-    };
+    	nixosConfigurations = {
+    		nixosadminpc = nixpkgs.lib.nixosSystem {
+        		system = "x86_64-linux";
+        		modules = [
+          			./configuration.nix
+	  			home-manager.nixosModules.home-manager {
+    					home-manager.useGlobalPkgs = true;
+    					home-manager.useUserPackages = true;
+    					home-manager.users.swarsel = import ./home.nix;
+  				}
+        		];
+		};
+      	};
+    
+    	# some future setup that only uses home manager on a non NixOS machine
+    	homeConfigurations = {
+            	homeMachine = home-manager.lib.homeManagerConfiguration {
+                	# Note: I am sure this could be done better with flake-utils or something
+                	pkgs = import nixpkgs { system = "x86_64-linux"; };
+                	modules = [ ./home.nix ]; # Defined later
+            	};
+        };
+    
   };
 }
