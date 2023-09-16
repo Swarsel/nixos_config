91f3355f50c9731bf91d5daf47380c91aef489fd
Swarsel <leon.schwarzaeugl@gmail.com>
2023-09-16 22:29:31 +0200

    possibly broken config

--- flake.nix
+++ flake.nix
@@ -15,10 +15,11 @@
 	};
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = inputs@{ self, nixpkgs, home-manager }: {
     	nixosConfigurations = {
     		nixos = nixpkgs.lib.nixosSystem {
-        		system = "x86_64-linux";
+        	system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
         		modules = [
           			./profiles/lenovo/configuration.nix
 				        ./profiles/lenovo/greetd.nix
--- profiles/lenovo/configuration.nix
+++ profiles/lenovo/configuration.nix
@@ -30,6 +30,7 @@
 	};
 
   # make impure cli commands compatible with flakes
+  #nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.nixPath = [ "nixpkgs=/etc/channels/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" "/nix/var/nix/profiles/per-user/root/channels" ];
   environment.etc."channels/nixpkgs".source = inputs.nixpkgs.outPath;
 
